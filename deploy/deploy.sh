a0b9c215e5c9986f59b565051707d354818ba20d
root <root@ip-172-31-16-49.us-west-1.compute.internal>
2021-06-21 00:59:25 +0000

    updating push and deploy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,7 +3,7 @@
 
 
 export IMAGE=maven-test
-export TAG=$BUILD_TAG
+export TAG=$BUILD_NAME
 export PASS=$PASS
 
 docker login -u barnali1986 -p $PASS
--- push/push.sh
+++ push/push.sh
@@ -10,8 +10,8 @@ IMAGE="maven-test"
 echo "****loggig in ****"
 docker login -u barnali1986 -p $PASS
 echo "****Tagging Image**"
-docker tag $IMAGE:$BUILD_TAG barnali1986/$IMAGE:$BULD_TAG
+docker tag $IMAGE:$BUILD_NUMBER  barnali1986/$IMAGE:$BUILD_NUMBER
 
 echo "*****Pushing images***"
-docker push barnali1986/$IMAGE:$BUILD_TAG
+docker push barnali1986/$IMAGE:$BUILD_NUMBER
 
